b08fb7010af8920b507c7d5bfa52fb559882d541
aphilliard <46166005+aphilliard@users.noreply.github.com>
2019-01-24 23:24:33 -0500

    Yet another iteration
    Fun with Python!

--- hello-world.py
+++ hello-world.py
@@ -1,10 +1,15 @@
-# hello world!
+hello = True
 
-a = True
-
-while a == True:
-    print('Hello World!')
-    b = input('Goodbye world?')
-    if b == 'y':
-        a = False
-        print('Bye!')
+while hello:
+	print('Hello, world!')
+	bye = input('Goodbye, world? ')
+	if bye == 'y' or bye == 'yes':
+		hello = False
+		print('Bye!')
+	elif bye == 'n' or bye == 'no':
+		continue
+	else:
+		bye = input('Please type "yes" or "no." ')
+		if bye == 'yes' or bye == 'y':
+			print('Bye!')
+			hello = False
